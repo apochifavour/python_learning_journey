6032b84f2cadbffec46bd39f79eb57a974587385
apochifavour <apochifavourogwa@gmail.com>
2026-09-21 16:15:10 +0100

    my second batch

---
+++ Part 1 — Programming & Python Foundations/solution/b2.py
@@ -0,0 +1,27 @@
+# MY WALLET
+
+print("====================")
+print("     MY WALLET")
+print("====================")
+
+print("Starting Balance:")
+print("₦10000")
+
+print("Deposit:")
+print("₦5000")
+
+print("Balance After Deposit:")
+print("₦" + str(10000 + 5000))
+
+
+print("Product:")         
+print("Notebook")               
+
+print("Price:")    
+print("₦2000")
+
+print("Quantity:")
+print(4)
+
+print("Total:")
+print("₦" + str(2000 * 4))
