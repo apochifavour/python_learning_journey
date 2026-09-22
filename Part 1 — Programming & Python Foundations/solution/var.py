4569008719c027ffbcef174e6ba54b0a701d30dd
apochifavour <apochifavourogwa@gmail.com>
2026-09-22 16:35:11 +0100

    my var work

---
+++ Part 1 — Programming & Python Foundations/solution/var.py
@@ -0,0 +1,46 @@
+product = "Notebook"
+price = 2000
+quantity = 4
+
+print(product)
+print(price)
+print(quantity)
+print(price * quantity)
+
+price = 5000
+quantity = 3
+
+total = price * quantity
+print(total)
+# store
+product_name = "Backpack"
+price = 15000
+quantity = 3
+
+total = price * quantity
+
+print(product_name)
+print(price)
+print(quantity)
+print(total)
+
+# wallet
+user_name = "Alex"
+balance = 30000
+deposit = 12000
+new_balance = balance + deposit
+
+print(user_name)
+print(balance)
+print(deposit)
+print(new_balance)
+
+age = 25
+price = 99.99
+name = "Python"
+is_learning = True
+
+print(type(age))
+print(type(price))
+print(type(name))
+print(type(is_learning))
