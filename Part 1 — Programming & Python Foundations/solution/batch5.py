1eeeaf57933820466922cca580aa5cef8514c767
apochifavour <apochifavourogwa@gmail.com>
2026-09-23 16:48:58 +0100

    batch5 work

---
+++ Part 1 — Programming & Python Foundations/solution/batch5.py
@@ -0,0 +1,24 @@
+name = "chris"
+age = 20
+print("Name:", name)
+print("Age:", age)
+name = input("Enter your name: ")
+age = input("Enter your age:")
+
+age = int(input("Enter your age: "))
+
+print(age + 5)
+price = float(input("Enter product price: "))
+
+name = input("Name: ")
+
+age = int(input("Age: "))
+
+price = float(input("Price: "))
+
+
+name = input("Enter your name: ")
+print("Hello " + name)
+
+age = input("Enter your age: ")
+print("Your age is " + age)
